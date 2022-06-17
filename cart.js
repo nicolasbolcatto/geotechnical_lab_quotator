84a94a6158728eefa74a66a03f61fc6dd902e0f7
nicolasbolcatto <nicobolcatto@gmail.com>
2022-06-17 14:31:30 -0300

    Added PDF export functionality and Toastify alerts

--- cart.js
+++ cart.js
@@ -1,53 +1,57 @@
-function saveItemsLocalStorage(array){
-    localStorage.setItem("array", JSON.stringify(array));
+
+//Function to search item by id
+function searchItem(id) {
+    fetch('./database.json').then(response => {
+        return response.json();
+    }).then(database => {
+        const item = database.find(x => x.id == id)
+        return item.id
+    }).catch(err => {
+        console.log("Something failed, unable to load items database");
+    });
 }
 
-function loadItemsLocalStorage(){
-    return JSON.parse(localStorage.getItem("array"));
-}
+//Function to load items in cart (stored in LS)
 
-//Funcion para buscar un item por su id
-
-function searchItem(id){
-    let items = loadItemsLocalStorage();
-    return items.find(x => x.id == id)
-}
-
-//Funcion para cargar los items del carrito alojados en la LS
-
-function loadCartItems(){
+function loadCartItems() {
     let cart = JSON.parse(localStorage.getItem("cart")) || []; //Uso operador OR
     return cart;
 }
 
-//Funcion para agregar un item al carrito
+//Function to add item to cart
 
-function addToCart(id){
-    
-    let itemsInCart = loadCartItems();
-    const position = itemsInCart.findIndex(element => element.id == id);
+function addToCart(id) {
+    fetch('./database.json').then(response => {
+        return response.json();
+    }).then(database => {
 
-    if(position === -1){
-        const item = searchItem(id);
-        item.quantity = 1;
-        itemsInCart.push(item);
-    } else {
-        itemsInCart[position].quantity++;   //Uso sugar syntax
-    }
+        let itemsInCart = loadCartItems();
+        const position = itemsInCart.findIndex(element => element.id == id);
+        if (position === -1) {
+            const item = database[id - 1];
+            item.quantity = 1;
+            itemsInCart.push(item);
+        } else {
+            itemsInCart[position].quantity++; //sugar syntax
+        }
 
-    localStorage.setItem("cart",JSON.stringify(itemsInCart));
-    updateCartButton();
+        localStorage.setItem("cart", JSON.stringify(itemsInCart));
+        updateCartButton();
+
+    }).catch(err => {
+        console.log("Something failed, unable to load items database");
+    });
 }
 
-//Funcion para eliminar el carrito
+//Funcion to delete cart
 
-function deleteCart(){
+function deleteCart() {
     Swal.fire({
         icon: 'warning',
-        title: 'Atención!',
-        text: 'Está seguro de eliminar todos los items?',
+        title: 'Warning!',
+        text: 'Are you sure? All items will be deleted from the cart',
         showDenyButton: true,
-        confirmButtonText: 'Si',
+        confirmButtonText: 'Yes',
         denyButtonText: 'No',
         customClass: {
             actions: 'my-actions',
@@ -55,99 +59,161 @@ function deleteCart(){
             confirmButton: 'order-2',
             denyButton: 'order-3',
         }
-        }).then((result) => {
+    }).then((result) => {
         if (result.isConfirmed) {
             localStorage.removeItem("cart");
             updateCartButton();
             renderSelectedItems();
         }
-        })
-
-
-    
+    })
 }
 
-//Funcion para eliminar un item del carrito
+//Function to delete single item from cart
 
-function deleteSingleItem(id){
+function deleteSingleItem(id) {
     let itemsInCart = loadCartItems();
     let itemsInCartUpdate = itemsInCart.filter(x => x.id != id);
-    localStorage.setItem("cart",JSON.stringify(itemsInCartUpdate));
+    localStorage.setItem("cart", JSON.stringify(itemsInCartUpdate));
+    Toastify({
+        text: "Item deleted successfully!",
+        duration: 2500,
+        gravity: 'bottom',
+        position: 'right',
+        className: 'notificacion my-toast',
+        style: {
+            background: "linear-gradient(to top right, lightgreen, green)",
+          }
+    }).showToast();
     updateCartButton();
     renderSelectedItems();
 }
 
-//Funcion para reducir en una unidad un item del carrito
+//Function to reduce quantity of single item by 1
 
-function reduceQuantity(id){
+function reduceQuantity(id) {
     const itemsInCart = loadCartItems();
     const position = itemsInCart.findIndex(element => element.id == id);
-    itemsInCart[position].quantity != 0 && itemsInCart[position].quantity--; //Utilizo operador logico AND y sugar syntax
-    localStorage.setItem("cart",JSON.stringify(itemsInCart));
+    itemsInCart[position].quantity != 0 && itemsInCart[position].quantity--; // AND & sugar syntax
+    localStorage.setItem("cart", JSON.stringify(itemsInCart));
     updateCartButton();
     renderSelectedItems();
 }
 
-//Funcion para aumentar en una unidad un item del carrito
+//Function to increase quantity of single item by 1
 
-function increaseQuantity(id){
+function increaseQuantity(id) {
     const itemsInCart = loadCartItems();
     const position = itemsInCart.findIndex(element => element.id == id);
-    itemsInCart[position].quantity++;  //Uso sugar syntax
-    localStorage.setItem("cart",JSON.stringify(itemsInCart));
+    itemsInCart[position].quantity++; //Uso sugar syntax
+    localStorage.setItem("cart", JSON.stringify(itemsInCart));
     updateCartButton();
     renderSelectedItems();
 }
 
-//Funcion que actualiza el numero de items diferentes en el carrito
+//Function to update cart button number of items
 
-function updateCartButton(){
+function updateCartButton() {
     let itemsInCart = loadCartItems();
-    let content=`<button type="button" class="btn position-relative fs-2 mx-4">Ir al carrito<span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">${itemsInCart.length}</span></button>`;
-    if (document.getElementById("go-cart")){
+    let content = `<button type="button" class="btn position-relative fs-2 mx-4">Go to cart<span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">${itemsInCart.length}</span></button>`;
+    if (document.getElementById("go-cart")) {
         document.getElementById("go-cart").innerHTML = content;
     }
-    
 }
 
-//Funcion que muestra los elementos seleccionados del carrito
+//Function to render selected elements in cart.html
 
-function renderSelectedItems(){
-    if (document.getElementById("main-cart")){
+function renderSelectedItems() {
+    if (document.getElementById("main-cart")) {
         let selected_items = loadCartItems();
         document.querySelector("#added-items").innerHTML = "";
         let counter = 0;
         let total = 0;
-        if (selected_items.length == 0){
-            let no_content = "<p class='fs-2 text-center bg-dark p-3'>No existen elementos seleccionados!</p>";
+        if (selected_items.length == 0) {
+            let no_content = "<p class='fs-2 text-center bg-dark p-3'>No items selected!</p>";
             document.getElementById("main-cart").innerHTML = no_content;
         } else {
 
-            for (const item of selected_items){
-                counter +=1;
+            for (const item of selected_items) {
+                counter += 1;
                 let added_item = document.createElement("tr");
                 added_item.setAttribute("id", `added-item-${counter}`);
-                let {id,price,quantity,description} = item;  //Uso desestructuracion
-                let calculate_price = price * quantity;
+                let { id, name, price, quantity} = item; //Destructuration
+                let calculate_price = Math.round((price * quantity *100))/100;
                 added_item.innerHTML = `
-                        <th class="fs-3" scope="row">${counter}</th>
-                                    <td class="fs-3">${description}</td>
+                        <td id="counter-${counter}" class="fs-3" scope="row">${counter}</td>
+                                    <td id="name-${counter}" class="fs-3">${name}</td>
                                     <td class="fs-3 text-center align-middle"><button id="reduce" class="btn btn-danger text-white mx-2 fs-3 fw-bold" onclick="reduceQuantity(${id})">-</button>${quantity}<button id="increase" class="btn btn-success text-white mx-2 fs-3 fw-bold" onclick="increaseQuantity(${id})">+</button></td>
-                                    <td class="fs-3 text-center align-middle">${price}</td>
-                                    <td class="fs-3 text-center align-middle">${calculate_price}</td>
+                                    <td id="unit-price-${counter}" class="fs-3 text-center align-middle">${price}</td>
+                                    <td id="total-price-${counter}" class="fs-3 text-center align-middle">${calculate_price}</td>
                                     <td class="fs-3 align-middle" id="delete-item-${counter}"><button class="btn btn-danger delete fw-bold"><a id="del-button" href="#" class="text-white text-decoration-none fs-3" onclick="deleteSingleItem(${id})">X</a></button></td>`;
-                                    added_item.setAttribute("class", "fs-3");
+                added_item.setAttribute("class", "fs-3");
                 document.querySelector("#added-items").appendChild(added_item);
                 total += calculate_price;
                 total_price = document.getElementById("total-price");
                 total_price.innerHTML = `Total = $ ${total}`;
             }
         }
-        
-        
     }
 }
 
-document.getElementById("delete-cart").addEventListener("click",deleteCart);
+document.getElementById("delete-cart").addEventListener("click", deleteCart);
 updateCartButton();
 renderSelectedItems();
+
+//PDF Generation
+const downloadButton = document.getElementById("download");
+downloadButton.addEventListener("click", () =>{
+    const client = document.getElementById("client").value;
+    const project = document.getElementById("project").value;
+    if (client == "" || project ==""){
+        return alert("Please complete the required fields")
+    }
+    window.jsPDF = window.jspdf.jsPDF;
+    var doc = new jsPDF();
+    doc.setFontSize(25);
+    doc.text("Geotechnical Lab Quotator", 50, 30);
+    doc.setFontSize(18);
+    
+    doc.text("Client: " + client, 10, 60);
+    doc.text("Project: " + project, 10, 70);
+
+    doc.setFontSize(12);
+    doc.text(document.getElementById("item-number").innerText, 10, 90);
+    doc.text(document.getElementById("item-description").innerText, 30, 90);
+    doc.text(document.getElementById("item-quantity").innerText, 180, 90);
+
+    
+    let selected_items = loadCartItems();
+    let j = 1;
+    let y = 90;
+    for (const item of selected_items){
+        doc.text(document.getElementById(`counter-${j}`).innerText,10,y+10);
+        doc.text(document.getElementById(`name-${j}`).innerText,30,y+10);
+        doc.text(item.quantity.toString(),190,y+10);
+
+        j++;
+        y += 10;
+    }
+
+    doc.text(document.getElementById("item-number").innerText, 10, y+20);
+    doc.text(document.getElementById("item-unit-price").innerText, 30, y+20);
+    doc.text(document.getElementById("item-total-price").innerText, 60, y+20);
+
+    y += 20;
+    j = 1;
+    for (const item of selected_items){
+        doc.text(document.getElementById(`counter-${j}`).innerText,10,y+10);
+        doc.text(document.getElementById(`unit-price-${j}`).innerText,30,y+10);
+        doc.text(document.getElementById(`total-price-${j}`).innerText,60,y+10);
+
+        j++;
+        y += 10;
+    }
+
+    doc.setFontSize(18);
+    doc.text(document.getElementById("total-price").innerText,10,y+20);
+    doc.setFontSize(12);
+    doc.text("Geotechnical Lab Quotator is Developed by Nico Bolcatto",10,y+40);
+    doc.text("nicobolcatto@gmail.com",10,y+50);
+    doc.save("a4.pdf");
+})
--- main.js
+++ main.js
@@ -1,60 +1,193 @@
-
-
 fetch('./database.json').then(response => {
     return response.json();
-  }).then(database => {
-    let content = "";
-    //Render items
+}).then(database => {
 
-    for (const item of database){
-        let {id,name,price,standards,description,picture} = item; //Uso desestructuracion
-        content += `
-        <div class="col">
-            <div class="card h-100">
-                <img id="item-img" src="/img/${picture}" class="card-img-top" alt="${name}">
-                <div class="card-body bg-dark">
-                    <h5 class="card-title fs-2">${name}</h5>
-                    <p class="lead fs-3">Precio: $ ${price}</p>
-                    <p class="card-text fs-4">Normas de aplicación: ${standards}</p>
-                    <p class="card-text fs-4">${description}</p>
-                </div>
-                <div class="card-footer bg-dark text-center">
-                <a href="#" class="btn btn-primary fs-2 m-3" onclick="addToCart(${id})"> Agregar </a>
-                <a href="#" id="desc-button-${id}"class="btn btn-success fs-2 m-3"> Ver descripcion </a>
-                </div>
-            </div>
-        </div>
-        `;
-    }
-    
-    document.getElementById("grid").innerHTML = content;
 
-    //Modals de descripcion
+    //Search bar, description modals & render elements
 
-    for (let i = 0; i <= database.length - 1; i++) {
-        let items = loadItemsLocalStorage();
-        let Button = document.getElementById("desc-button-" + (i + 1));
-        let {name,description,standards,unit,picture} = items[i];
-        Button.addEventListener('click',()=>{
+    const userCardTemplate = document.querySelector("[data-item-template]");
+    const userCardContainer = document.querySelector("[data-item-card-container]");
+    const searchInput = document.querySelector("[data-search]");
+
+    let items = []
+    items = database.map(item => {
+        const card = userCardTemplate.content.cloneNode(true).children[0];
+        const header = card.querySelector("[data-header]");
+        const body = card.querySelector("[data-body]");
+        const footer = card.querySelector("[data-footer]");
+        let { id, category, name, price, description, standards, unit, picture, materialRequired } = item;
+        header.innerHTML = `<img id="item-img" src="/img/${picture}" class="card-img-top" alt="${name}">`
+        body.innerHTML = `<div><h5 class="card-title fs-2">${name}</h5></div>
+        <div><p class="lead fs-3">Price: $ ${price}</p></div>`
+        footer.innerHTML = `<a href="#" id="add-button-${id}" class="btn btn-add fs-2 m-3"> Add to cart </a>
+        <a href="#" id="desc-button-${id}" class="btn btn-desc fs-2 m-3"> See description </a>`
+        userCardContainer.append(card);
+
+        //Description modals using SweetAlert
+
+        let button = document.getElementById(`desc-button-${id}`);
+        button.addEventListener('click', () => {
 
             Swal.fire({
                 title: `${name}`,
                 width: 500,
                 html: `<p class="fs-3 py-2">${description}</p>
-                <p class="fs-3 py-2">Normativa: ${standards}</p>
-                <p class="fs-3 py-2">Unidad: ${unit}</p>`,
+                <p class="fs-3 py-2">Standard: ${standards}</p>
+                <p class="fs-3 py-2">Sample type: ${unit}</p>
+                <p class="fs-3 py-2">Material required: ${materialRequired}`,
                 imageUrl: `img/${picture}`,
                 imageWidth: 300
             })
         })
-    }
 
-   
-  }).catch(err => {
-    console.log("No fue posible cargar la lista de items");
-  });
+        //Event listeners for adding button
 
+        let addButton = document.getElementById(`add-button-${id}`);
+        addButton.addEventListener('click', () => {
+            addToCart(id);
+            Toastify({
+                text: "Item added successfully!",
+                duration: 2500,
+                gravity: 'bottom',
+                position: 'right',
+                className: 'notificacion my-toast',
+                style: {
+                    background: "linear-gradient(to top right, lightgreen, green)",
+                  }
+            }).showToast();
+            
 
+        })
 
+        return { name: name, element: card, category: category };
+    })
 
+    //Event listener for search bar
+    searchInput.addEventListener("input", (e) => {
+        const value = e.target.value.toLowerCase()
+        items.forEach(item => {
+            let isVisible = item.name.toLowerCase().includes(value);
+            item.element.classList.toggle("hide", !isVisible);
+        })
+    })
 
+    //Event listeners for category selection
+    let categoryButtonAll = document.getElementById("all");
+    categoryButtonAll.addEventListener("click", () => {
+        items.forEach(item => {
+            item.element.classList.remove("hide");
+            })
+            
+        })
+
+    let categoryButtonClassification = document.getElementById("classification")
+    categoryButtonClassification.addEventListener("click", () =>{
+        items.forEach(item => {
+            if (item.category != "Classification"){
+                item.element.classList.add("hide");
+            } else {
+                item.element.classList.remove("hide");
+            }
+            
+        })
+    })
+
+    let categoryButtonCompaction = document.getElementById("compaction");
+    categoryButtonCompaction.addEventListener("click", () =>{
+        items.forEach(item => {
+            if (item.category != "Compaction"){
+                item.element.classList.add("hide");
+            } else {
+                item.element.classList.remove("hide");
+            }
+            
+        })
+    })
+
+    let categoryButtonConsolidation = document.getElementById("consolidation");
+    categoryButtonConsolidation.addEventListener("click", () =>{
+        items.forEach(item => {
+            if (item.category != "Consolidation"){
+                item.element.classList.add("hide");
+            } else {
+                item.element.classList.remove("hide");
+            }
+            
+        })
+    })
+
+    let categoryButtonTotalStress = document.getElementById("total-stress");
+    categoryButtonTotalStress.addEventListener("click", () =>{
+        items.forEach(item => {
+            if (item.category != "Total Stress"){
+                item.element.classList.add("hide");
+            } else {
+                item.element.classList.remove("hide");
+            }
+            
+        })
+    })
+
+    let categoryButtonEffectiveStress = document.getElementById("effective-stress");
+    categoryButtonEffectiveStress.addEventListener("click", () =>{
+        items.forEach(item => {
+            if (item.category != "Effective Stress"){
+                item.element.classList.add("hide");
+            } else {
+                item.element.classList.remove("hide");
+            }
+            
+        })
+    })
+
+    let categoryButtonPermeability = document.getElementById("permeability");
+    categoryButtonPermeability.addEventListener("click", () =>{
+        items.forEach(item => {
+            if (item.category != "Permeability"){
+                item.element.classList.add("hide");
+            } else {
+                item.element.classList.remove("hide");
+            }
+            
+        })
+    })
+
+    let categoryButtonRock = document.getElementById("rock");
+    categoryButtonRock.addEventListener("click", () =>{
+        items.forEach(item => {
+            if (item.category != "Rock"){
+                item.element.classList.add("hide");
+            } else {
+                item.element.classList.remove("hide");
+            }
+            
+        })
+    })
+
+    let categoryButtonChemical = document.getElementById("chemical");
+    categoryButtonChemical.addEventListener("click", () =>{
+        items.forEach(item => {
+            if (item.category != "Chemical"){
+                item.element.classList.add("hide");
+            } else {
+                item.element.classList.remove("hide");
+            }
+            
+        })
+    })
+
+    let categoryButtonOrganic = document.getElementById("organic")
+    categoryButtonOrganic.addEventListener("click", () =>{
+        items.forEach(item => {
+            if (item.category != "Organic"){
+                item.element.classList.add("hide");
+            } else {
+                item.element.classList.remove("hide");
+            }
+            
+        })
+    })
+
+}).catch(err => {
+    console.log("Something failed, unable to load items database");
+});
